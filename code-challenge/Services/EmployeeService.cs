1238d53364f478339110b2ba382117f4e0b9be4b
Courtney Dart <dart.courtney1@gmail.com>
2021-07-11 10:05:48 -0400

    Add project to repo.

---
+++ code-challenge/Services/EmployeeService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+
+
+        public Employee Create(Employee employee)
+        {
+            if(employee != null)
+            {
+                _employeeRepository.Add(employee);
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return employee;
+        }
+
+        public Employee GetById(string id)
+        {
+            if(!String.IsNullOrEmpty(id))
+            {
+                return _employeeRepository.GetById(id);
+            }
+
+            return null;
+        }
+
+        // This method returns the numberOfReports for an employee and all of their direct reports.
+        public ReportingStructure GetReporting(string id)
+        {
+            int count = 0;
+            var employee = GetById(id);
+            ReportingStructure reportingStructure = new ReportingStructure();
+            reportingStructure.employee = employee;
+            RecursiveReports(employee, ref count);
+            reportingStructure.numberOfReports = count;
+            return reportingStructure;
+        }
+
+        // This method iterates through the currentEmployee, adding one to the count each time
+        // the currentEmployee has DirectReports.
+        private void RecursiveReports(Employee employee, ref int count)
+        {
+            if (employee.DirectReports == null)
+            {
+                return;
+            }
+            if (employee.DirectReports != null)
+            {
+                foreach (var currentEmployee in employee.DirectReports)
+                {
+                    count++;
+                    RecursiveReports(currentEmployee, ref count);
+                }
+            }
+        }
+
+        public Employee Replace(Employee originalEmployee, Employee newEmployee)
+        {
+            if(originalEmployee != null)
+            {
+                _employeeRepository.Remove(originalEmployee);
+                if (newEmployee != null)
+                {
+                    // ensure the original has been removed, otherwise EF will complain another entity w/ same id already exists
+                    _employeeRepository.SaveAsync().Wait();
+
+                    _employeeRepository.Add(newEmployee);
+                    // overwrite the new id with previous employee id
+                    newEmployee.EmployeeId = originalEmployee.EmployeeId;
+                }
+                _employeeRepository.SaveAsync().Wait();
+            }
+
+            return newEmployee;
+        }
+    }
+}
